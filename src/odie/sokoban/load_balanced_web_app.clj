27965a0011fbeb3085218d25a84c5599b187c2e0
Jonathan Shieh <jonathan.shieh@gmail.com>
2022-05-30 20:55:09 +0800

    [WIP] Trying to get EC2 backed ECS cluster to create without error

--- src/odie/sokoban/load_balanced_web_app.clj
+++ src/odie/sokoban/load_balanced_web_app.clj
@@ -3,7 +3,9 @@
             [odie.sokoban.aws-utils :as au]
             [odie.sokoban.utils :as u]
             [cognitect.aws.client.api :as aws]
-            [odie.sokoban.globals :as g]))
+            [odie.sokoban.globals :as g]
+            [odie.sokoban.dev :as dev]
+            ))
 
 ;; Steps
 ;; 1. Setup roles
@@ -26,11 +28,20 @@
    :Tags (au/->tags {:sokoban-application app-name})})
 
 
-(defn setup-common-infrastructure--req-data [params]
+(defn setup-ECS-Fargate-infrastructure--req-data [params]
   (let [{:keys [AppName EnvironmentName]} params]
     {:StackName (format "%s-%s" AppName EnvironmentName)
      :Capabilities ["CAPABILITY_NAMED_IAM"]
-     :TemplateBody (slurp (io/resource "cf-templates/shared.yml"))
+     :TemplateBody (slurp (io/resource "cf-templates/ECS-Fargate.yml"))
+     :Parameters (au/->params params)
+     :Tags (au/->tags {:sokoban-application AppName
+                       :sokoban-environment EnvironmentName})}))
+
+(defn setup-ECS-EC2-infrastructure--req-data [params]
+  (let [{:keys [AppName EnvironmentName]} params]
+    {:StackName (format "%s-%s" AppName EnvironmentName)
+     :Capabilities ["CAPABILITY_NAMED_IAM"]
+     :TemplateBody (slurp (io/resource "cf-templates/ECS-EC2.yml"))
      :Parameters (au/->params params)
      :Tags (au/->tags {:sokoban-application AppName
                        :sokoban-environment EnvironmentName})}))
@@ -66,28 +77,44 @@
   (aws/invoke cf {:op :ListStackResources
                   :request {:StackName stackname}}))
 
+(defn resource-created? [resource-summary]
+  (= (:ResourceStatus resource-summary) "CREATE_COMPLETE"))
+
+(defn resource-deleted? [resource-summary]
+  (let [status (:ResourceStatus resource-summary)]
+    (or (= status "DELETE_COMPLETE")
+        (= status "DELETE_SKIPPED"))))
+
 (defn cf-watch-for-stack-completion [cf stackname]
-  (let [start-time (System/nanoTime)]
-    (loop []
-        (let [reply (cf-fetch-stack-status cf stackname)
-              summaries (:StackResourceSummaries reply)]
+  (let [start-time (System/nanoTime)
+        result (loop []
+                 (let [reply (cf-fetch-stack-status cf stackname)
+                       summaries (:StackResourceSummaries reply)]
 
-          (->> summaries
-               (map #(select-keys % [:LogicalResourceId :ResourceStatus]))
-               clojure.pprint/print-table
-               )
+                   ;; If we can't fetch the summaries for some reason...
+                   (if-not summaries
 
-          ;; (doseq [item summaries]
-          ;;   (println (:LogicalResourceId item) (:ResourceStatus item)))
+                     ;; Just return whatever AWS sent us
+                     reply
 
-          ;; If there are still resources that are not done being
-          ;; created/updated, wait a few seconds and try again
-          (if (some #(not= (:ResourceStatus %) "CREATE_COMPLETE") summaries)
-            (do
-              (Thread/sleep 5000)
-              (recur)))))
+                     ;; We can fetch the summaries...
+                     (do
+                       ;; Print it
+                       (->> summaries
+                            (map #(select-keys % [:LogicalResourceId :ResourceStatus]))
+                            clojure.pprint/print-table
+                            )
 
-    (println (format "Waited %.2f seconds" (float (/ (- (System/nanoTime) start-time) 1000000000))))))
+                       ;; Keep looping every few seconds until everything has either been
+                       ;; created or deleted (in case there is an error).
+                       (if-not (or (every? #(resource-created? %) summaries)
+                                   (every? #(resource-deleted? %) summaries))
+                         (do
+                           (Thread/sleep 5000)
+                           (recur)))))))]
+
+    (println (format "Waited %.2f seconds" (float (/ (- (System/nanoTime) start-time) 1000000000))))
+    result))
 
 (defn setup-load-balanced-web-service--req-data [params]
   ;; FIXME!!! The way we gather and destructure the params is a bit
@@ -106,13 +133,59 @@
                        })}))
 
 (comment
+  (dev/dev-start-app!)
+
+  ;;-----------------------------------------------
+  ;;
+  ;; Explore KMS keys
+  (def kms (aws/client {:api :kms
+                       :credentials-provider @g/credentials-provider}))
+
+  (aws/doc kms :ListKeys)
+
+  (def aws-keys
+    (->> (aws/invoke kms {:op :ListKeys})
+         :Keys
+         (map #(:KeyId %))))
+
+  (aws/doc kms :DescribeKey)
+
+  (def aws-keys-details
+    (->> aws-keys
+         (map #(aws/invoke kms {:op :DescribeKey
+                                :request {:KeyId %}}))))
+
+  ;;-----------------------------------------------
+  ;;
+  ;; Explore EC2 KeyPairs
+  (def ec2 (aws/client {:api :ec2
+                        :credentials-provider @g/credentials-provider}))
+
+  (aws/validate-requests ec2)
+
+  (aws/doc ec2 :DescribeKeyPairs)
+
+  (aws/invoke ec2 {:op :DescribeKeyPairs})
+
+  (aws/doc ec2 :CreateKeyPair)
+
+  (def create-key-reply
+    (aws/invoke ec2 {:op :CreateKeyPair
+                     :request {}}))
+
+  ;;-----------------------------------------------
+  ;;
+  ;; Interactively operate CloudFormation
   (def cf (aws/client {:api :cloudformation
                        :credentials-provider @g/credentials-provider}))
 
   (aws/validate-requests cf)
 
-  ;; Step 1: Setup roles
+  (g/set-app-name! "orange")
 
+
+
+  ;; Step 1: Setup roles
   (def create-result
     (let [req (setup-roles--req-data (select-keys @g/app-context [:app-name :account-id]))
           spec? (u/on-spec? (aws/request-spec-key cf :CreateStack) req)]
@@ -123,7 +196,7 @@
   (if (:StackId create-result)
     (cf-watch-for-stack-completion cf (:StackId create-result)))
 
-  ;; Step 2: Setup common infrastructure
+  ;; Step 2a: Setup ECS Fargate cluster
   (def create-result
     (let [environment "test"
           app-name (:app-name @g/app-context)
@@ -145,7 +218,7 @@
                   :CreateHTTPSListener "false"
                   :ServiceDiscoveryEndpoint (format "%s.%s.%s" environment app-name "local")
                   }
-          req (setup-common-infrastructure--req-data params)
+          req (setup-ECS-Fargate-infrastructure--req-data params)
           spec? (u/on-spec? (aws/request-spec-key cf :CreateStack) req)]
 
       (if (true? spec?)
@@ -158,6 +231,46 @@
     (cf-watch-for-stack-completion cf (:StackId create-result))
     create-result)
 
+  ;; Step 2b: Setup ECS EC2 cluster
+  (def create-result
+    (let [environment "test"
+          app-name (:app-name @g/app-context)
+          account-id (:account-id @g/app-context)
+          params {:AppName app-name
+                  :EnvironmentName environment
+                  :ALBWorkloads "api"
+                  ;; EFSWorkloads:
+                  ;; Type: String
+                  ;; NATWorkloads:
+                  ;; Type: String
+                  :ToolsAccountPrincipalARN (str "arn:aws:iam::" account-id ":root")
+                  ;; AppDNSName:
+                  ;; Type: String
+                  ;; AppDNSDelegationRole:
+                  ;; Type: String
+                  ;; Aliases:
+                  ;; Type: String
+                  :CreateHTTPSListener "false"
+                  :ServiceDiscoveryEndpoint (format "%s.%s.%s" environment app-name "local")
+                  :EC2InstanceType "t3.small"
+                  :AsgMinSize "0"
+                  :AsgMaxSize "5"
+                  :AsgDesiredCapacity "1"
+                  }
+          req (setup-ECS-EC2-infrastructure--req-data params)
+          spec? (u/on-spec? (aws/request-spec-key cf :CreateStack) req)]
+
+      (if (true? spec?)
+        (cf-stack-ensure cf req)
+        spec?)
+      ))
+
+  ;; Watch and wait for the creation to complete
+  (if (:StackId create-result)
+    (cf-watch-for-stack-completion cf (:StackId create-result))
+    create-result)
+
+  (def create-result nil)
 
   ;; Step 3: Setup load-balanced web service
   (def create-result
